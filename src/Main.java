c675573cbfba86de191e4c0f8b4a528f1d0458db
Dachi Laperashvili <98727379+Dachi-Laperashvili@users.noreply.github.com>
2023-04-26 16:35:41 +0400

    added version1 of mortgage calculator

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,36 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args){
-        int principal = (int) readNumber("Principal: ",1000,1000000);
-        float annualInterest = (float) readNumber("Annual Interest Rate: ",1,30);
-        byte years = (byte) readNumber("Period (years): ",1,30);
-        Mortgage client = new Mortgage(principal,annualInterest,years);
-        client.printMortgage(principal,annualInterest,years);
-        client.printPaymentSchedule(principal,annualInterest,years);
-    }
-    public static double readNumber(String prompt, double min, double max) {
+    public static void main(String[] args) {
+        // Mortgage calculator
         Scanner input = new Scanner(System.in);
-        double number;
-        while (true) {
-            System.out.print(prompt);
-            number = input.nextFloat();
-            if (number >= min && number <= max) {
-                break;
-            }
-            System.out.println("Enter a value between " + min + " and " + max);
+        // asking user for principle
+        System.out.println("Enter your principle (1k-1m)");
+        int principle = input.nextInt();
+        // if principle isn't in 1k-1m range asking user to enter a valid number
+        while(principle < 1000 || principle > 1000000 ){
+            System.out.println("Please enter a valid number");
+            principle = input.nextInt();
         }
-        return number;
-    }
+        // asking user for yearly rate (0;100)
+        System.out.println("Enter the yearly rate (0-100)");
+        float r = input.nextInt();
+        // if yearly rate isn't in 0-100 range asking user to enter a valid number
+        while (r < 0 || r > 100) {
+            System.out.println("Please enter a valid number");
+            r = input.nextFloat();
+        }
+        // asking user to enter the number of years
+        System.out.println("Enter the number of years (0-100)");
+        byte num = input.nextByte();
+        // if number of years isn't in 0-100 range asking user to enter a valid number
+        while (num < 0 || num > 100) {
+            System.out.println("Please enter a valid number");
+            num = input.nextByte();
+        }
+        float R = r * 12 * 100;
+        float N = num / 12;
+        double mortgage = principle * (R * Math.pow(1 + R,N)) / (Math.pow(1 + R, N) - 1);
 
+        System.out.println("Your monthly payment is " + mortgage);
+    }
 }
